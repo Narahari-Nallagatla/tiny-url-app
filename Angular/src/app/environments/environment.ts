bb51fb853e7c8f6cff131493318183cc383f9271
Narahari Nallagatla <nallagatlanarahari@gmail.com>
2026-05-02 21:02:51 +0530

    Changed the Angular environment.ts file, Added the Azure apiUrl

--- Angular/src/app/environments/environment.ts
+++ Angular/src/app/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tiny-url-demo.azurewebsites.net/api'
+  // apiUrl: 'https://tiny-url-demo.azurewebsites.net/api'
+  // apiUrl: 'https://localhost:7206/api'
+  apiUrl: 'https://app-tinyurl-6f730274.azurewebsites.net/api'
 };
